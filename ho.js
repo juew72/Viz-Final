8b2c3a29a1b5e7dd71675d1c43bf6c09245bfd66
juew72 <38595722+juew72@users.noreply.github.com>
2018-05-05 19:31:38 -0400

    Delete ho.js

--- ho.js
+++
@@ -1,6 +0,0 @@
-function changeToBlueColor(){
-  document.getElementById("titleRegister").style.color = "blue";
-}
-function changeToBlackColor(){
-  document.getElementById("titleRegister").style.color = "black";
-}
