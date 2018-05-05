78d4ee6902f1a7f4e234fd0daab08960502f16b4
juew72 <38595722+juew72@users.noreply.github.com>
2018-05-05 19:30:22 -0400

    Create ho.js

---
+++ ho.js
@@ -0,0 +1,6 @@
+function changeToBlueColor(){
+  document.getElementById("titleRegister").style.color = "blue";
+}
+function changeToBlackColor(){
+  document.getElementById("titleRegister").style.color = "black";
+}
