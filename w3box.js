8c54553dc9d9bb1b311045b40f88f315921c4ef0
juew72 <38595722+juew72@users.noreply.github.com>
2018-05-06 18:45:06 -0400

    Create w3box.js

---
+++ w3box.js
@@ -0,0 +1,8 @@
+function openy(Year) {
+    var i;
+    var x = document.getElementsByClassName("Year");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    document.getElementById(Year).style.display = "block";
+}
