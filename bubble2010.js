98368fa8cd4978d8c8b567707b6de5a356790a92
juew72 <38595722+juew72@users.noreply.github.com>
2018-05-06 07:17:08 -0400

    Update bubble2010.js

--- bubble2010.js
+++ bubble2010.js
@@ -62,7 +62,7 @@ var bubble = d3.pack(dataset)
     .size([diameter, diameter])
     .padding(1.5);
 
-var svg = d3.select("#chart2010")
+var svg = d3.select("div2010")
     .append("svg")
     .attr("width", diameter)
     .attr("height", diameter)
